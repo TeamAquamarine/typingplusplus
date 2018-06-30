2d80e12556c62aa4dae4c7ad1f37e6c5967d3fb1
Sharon Miller <sharon@sharonmillercreative.com>
2018-06-30 16:52:34 -0700

    worked on click handler

--- js/inputvalidation.js
+++ js/inputvalidation.js
@@ -1,1 +1,38 @@
 'use strict';
+var shake = document.getElementsByClassName('shake');
+debugger;
+shake.addEventListener('keypress', textValidation);
+debugger;
+
+function textValidation(event) {
+  console.log('alert working');
+  event.preventDefault();
+
+}
+
+
+// function click(event) {
+//   document.getElementsByClassName('shake').addEventListner('submit', function (event) {
+//   });
+//   event.preventDefault();
+// }
+// click();
+
+// // var shake = document.getElementsByClassName('shake');
+
+
+
+// function validation() {
+//   var shake = document.getElementsByClassName('shake');
+//   shake.classList += 'validation';
+//   if (validation.innertext == false) {
+//     shake.style.backgroundColor = '#ff4c4c';
+//   }
+//   // debugger;
+
+//   // validation.classList.add('error');
+
+//   preventDefault();
+// }
+
+// validation();
