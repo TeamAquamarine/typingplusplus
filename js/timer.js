bd328cfc610594a01c2050db7576d4cf6a0cee4e
MEGAN IVEY <printmeganivey@gmail.com>
2018-07-02 08:28:10 -0700

    disregard

---
+++ js/timer.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var totalTime = 10;
+var timerInterval;
+
+function countDownTimer() {
+  totalTime--;
+  var countdown = function () {
+    console.log(totalTime--);
+    if (totalTime < 0 ) {
+      clearInterval(timerInterval);
+    }
+  };
+  timerInterval = setInterval(countdown, 1000);
+}
+
+countDownTimer();
+
