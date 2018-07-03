7d2f263acc180479a00fe7d000fc90ae918dc990
Connor Crossley <cacrossley22@gmail.com>
2018-07-03 00:32:01 -0700

    formatting

--- js/timer.js
+++ js/timer.js
@@ -1,45 +1,6 @@
 'use strict';
 //global variables 
 var timeInterval;
-// var timerStartButton = document.getElementById('start');
-// var timerStopButton = document.getElementById('stop');
-
-// //bar timer count down
-// function countDownTimer() {
-//   totalTime--;
-//   var countdown = function () {
-//     console.log(totalTime--);
-//     if (totalTime < 0) {
-//       clearInterval(timerInterval);
-//     }
-//   };
-//   timerInterval = setInterval(countdown, 1000);
-// }
-// //start button event listener
-// timerStartButton.addEventListener('click', startTimerBar);
-// //animation for timer bar
-// function startTimerBar(event) {
-//   event.preventDefault();
-//   var animationTime = 'timerBar ' + totalTime + 's linear forwards';
-//   timerBar.style.animation = animationTime;
-//   console.log(timerBar);
-//   countDownTimer();
-// }
-// //stop button for timer
-// timerStopButton.addEventListener('click', stopTimerBar);
-
-// // Stop timer, pause animation
-// function stopTimerBar(event) {
-//   event.preventDefault();
-//   var animationTime = 'timerBar ' + totalTime + 's linear forwards paused';
-//   timerBar.style.animation = animationTime;
-//   clearInterval(timerInterval);
-// }
-
-// function resetTimerBar() {
-//   totalTime = 30;
-//   timerBar.style.animation = '';
-// }
 
 /***********************************
 *     Timer Object Constructor     *
@@ -59,7 +20,7 @@ Timer.prototype.startTimer = function () {
 Timer.prototype.countDown = function () {
   var _this = this;
   timeInterval = setInterval(function () {
-    if(_this.timeRemaining > 0){
+    if (_this.timeRemaining > 0) {
       _this.timeRemaining--;
       console.log(_this.timeRemaining);
     } else {
