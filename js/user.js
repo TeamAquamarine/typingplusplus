451c2b094b6f86ad3e57833ed0a7931627bd4eb5
Alex Hanson <alexlhanson@gmail.com>
2018-07-03 08:42:10 -0700

    Basic user constructor for merge

--- js/user.js
+++ js/user.js
@@ -1,6 +1,7 @@
 'use strict';
-
+//globals
 userArray = [];
+var userNode = document.getElementById('playerNameInput');
 
 //user constructor
 function User(name) {
@@ -15,6 +16,14 @@ User.prototype.calcScore = function() {};
 
 //When there is a handler for name input can create this function
 User.prototype.storeLocal = function() {};
+
+//listener/handler to create user objects
+userNode.addEventListener('submit', createUserObject);
+
+// function createUserObject(event){
+//   new User(event.target);
+// }
+
 
 //New instance to check its working
 var Fred = new User(Fred);
