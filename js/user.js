8b91269e3d4aa031186784644ce4dbc6938ffb3b
Connor Crossley <cacrossley22@gmail.com>
2018-07-03 13:52:32 -0700

    References #24. User object array saving in local storage

--- js/user.js
+++ js/user.js
@@ -1,7 +1,9 @@
 'use strict';
 //globals
-userArray = [];
 var userNode = document.getElementById('playerNameInput');
+
+// Grab users array from local storage or create an empty array if nothing is stored
+var userArray = JSON.parse(localStorage.getItem('users') || '[]');
 
 //user constructor
 function User(name) {
@@ -9,21 +11,27 @@ function User(name) {
   this.highScore = 0;
   this.level = 1;
   userArray.push(this);
+  localStorage.setItem('users', JSON.stringify(userArray));
 };
 
 //When there is consensus on calculating score can create this function
-User.prototype.calcScore = function() {};
+User.prototype.incrementScore = function() {
+  this.highScore += 1000000;
+};
 
 //When there is a handler for name input can create this function
-User.prototype.storeLocal = function() {};
+User.prototype.storeLocal = function() {
+  localStorage.setItem(JSON.stringify('users', this));
+};
 
 //listener/handler to create user objects
 userNode.addEventListener('submit', createUserObject);
 
-// function createUserObject(event){
-//   new User(event.target);
-// }
+function createUserObject(event){
+  new User(event.target.value);
+}
 
 
 //New instance to check its working
-var Fred = new User(Fred);
+var Fred = new User('Fred');
+console.log(userArray);
