e0fc399666cd79e33e70452ea022a5e6fc10d337
Alex Hanson <alexlhanson@gmail.com>
2018-07-04 10:52:33 -0700

    Closes #31 Add default users to populate high score table

--- js/user.js
+++ js/user.js
@@ -2,6 +2,7 @@
 //globals
 var userNode = document.getElementById('playerNameInput');
 var playButton = document.getElementById('playGameButton');
+var defaultScore = 999990;
 // Grab users array from local storage or create an empty array if nothing is stored
 var userArray = JSON.parse(localStorage.getItem('users') || '[]');
 
@@ -34,10 +35,40 @@ playButton.addEventListener('click', createUserObject);
 // Creates a new user and saves to local storage when name is inputted and play is clicked
 function createUserObject(event){
   event.preventDefault();
-  console.log(userNode.value);
   currentUser = new User(userNode.value);
   userArray.push(currentUser);
   localStorage.setItem('users', JSON.stringify(userArray));
-  console.log(currentUser);
   localStorage.setItem('currentUser', JSON.stringify(currentUser));
+}
+
+/********************************************************************************
+*         Instantiate default users for high score                              *
+********************************************************************************/
+
+//function to create a default user
+function createDefaultUser (name){
+  var defaultUser = new User(name);
+  defaultUser.highScore = defaultScore;
+  defaultScore++;
+  userArray.push(defaultUser);
+  localStorage.setItem('users', JSON.stringify(userArray));
+}
+
+//function to create set of default top 10 users
+function populateUserArray (defaultScore){
+  createDefaultUser('Jasper', defaultScore);
+  createDefaultUser('Declan', defaultScore);
+  createDefaultUser('Sadie', defaultScore);
+  createDefaultUser('Finley', defaultScore);
+  createDefaultUser('Brooklyn', defaultScore);
+  createDefaultUser('Gabe', defaultScore);
+  createDefaultUser('Evelyn', defaultScore);
+  createDefaultUser('Jameson', defaultScore);
+  createDefaultUser('Isla', defaultScore);
+  createDefaultUser('Nicholas', defaultScore);
+}
+
+//calls the populate default users in not in storage already
+if (!localStorage.getItem('users')){
+  populateUserArray();
 }
