26b95b16db3c6421bd8347cd4baf04647b0626de
Connor Crossley <34751792+Concross@users.noreply.github.com>
2018-07-06 08:45:00 -0700

    Update levelObject.js

--- js/levelObject.js
+++ js/levelObject.js
@@ -146,7 +146,7 @@ function winLevel() {
 
       var winMsg = 'Congratulations, ' + currentUser.name + '! You\'re promoted!';
       localStorage.setItem('highScoreMsg', JSON.stringify(winMsg));
-      window.location.href = 'typingplusplus.com/html/high_score.html';
+      window.location.href = 'https://www.typingplusplus.com/html/high_score.html';
 
     } else {
       levelArray[currentUser.level - 1].render();
